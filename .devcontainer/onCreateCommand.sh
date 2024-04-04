fef1c0f626f674a7ce319ac69bd60801ca0b39fa
Mike Szczys <mike@golioth.io>
2024-04-04 17:27:36 -0500

    devcontainer: test usb when run locally
    This test installs the SEGGER and Nordic programming tools, mounts the USB
    into the container, and runs in priviledged mode. It has been tested and
    working on Linux when the devcontainer is run locally using VScode.
    
    Signed-off-by: Mike Szczys <mike@golioth.io>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+apt update
+apt install wget libusb-1.0-0 -y
+
+## The following is based on nrf-docker: https://github.com/NordicPlayground/nrf-docker/blob/saga/Dockerfile
+# Nordic command line tools
+# Releases: https://www.nordicsemi.com/Products/Development-tools/nrf-command-line-tools/download
+NORDIC_COMMAND_LINE_TOOLS_VERSION="10-23-2/nrf-command-line-tools-10.23.2"
+NCLT_BASE="https://nsscprodmedia.blob.core.windows.net/prod/software-and-other-downloads/desktop-software/nrf-command-line-tools/sw/versions-10-x-x"
+ARCH_STR="linux-amd64"
+mkdir tmp && cd tmp
+wget "${NCLT_BASE}/${NORDIC_COMMAND_LINE_TOOLS_VERSION}_${ARCH_STR}.tar.gz"
+tar --no-same-owner -xzf *.tar.gz
+# Install included JLink
+mkdir -p /opt/SEGGER
+tar xzf JLink_*.tgz -C /opt/SEGGER
+mv /opt/SEGGER/JLink* /opt/SEGGER/JLink
+# Install nrf-command-line-tools
+cp -r ./nrf-command-line-tools /opt
+ln -s /opt/nrf-command-line-tools/bin/nrfjprog /usr/local/bin/nrfjprog
+ln -s /opt/nrf-command-line-tools/bin/mergehex /usr/local/bin/mergehex
+cd .. && rm -rf tmp
+
+## This is the Zephyr/Golioth specific setup
 west init -l app
 west update
 west zephyr-export
